d8abcccff320918c5ade293f19b9e3b85f03f8d6
fabio.dearcangelis <fabio.dearcangelis@linksmt.it>
2026-07-13 22:16:33 +0200

    Introdotto `RaritySelector` con distribuzione predefinita e test di validazione per selezione deterministica e copertura delle rarità.

---
+++ src/main/java/it/fantasytoolkitcore/core/model/RaritySelector.java
@@ -0,0 +1,28 @@
+package it.fantasytoolkitcore.core.model;
+
+import java.util.Random;
+
+public final class RaritySelector {
+
+    private final RarityTable rarityTable;
+
+    private RaritySelector(RarityTable rarityTable) {
+        this.rarityTable = rarityTable;
+    }
+
+    public static RaritySelector withDefaultDistribution() {
+        RarityTable rarityTable = RarityTable.builder()
+                .entry(Rarity.COMMON, 50)
+                .entry(Rarity.UNCOMMON, 25)
+                .entry(Rarity.RARE, 12)
+                .entry(Rarity.EPIC, 8)
+                .entry(Rarity.LEGENDARY, 5)
+                .build();
+
+        return new RaritySelector(rarityTable);
+    }
+
+    public Rarity select(Random random) {
+        return rarityTable.draw(random);
+    }
+}
---
+++ src/test/java/it/fantasytoolkitcore/core/model/RaritySelectorTest.java
@@ -0,0 +1,46 @@
+package it.fantasytoolkitcore.core.model;
+
+import java.util.EnumSet;
+import java.util.Random;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RaritySelectorTest {
+
+    @Test
+    void selectReturnsNonNullRarity() {
+        RaritySelector selector = RaritySelector.withDefaultDistribution();
+
+        Rarity selected = selector.select(new Random(0L));
+
+        assertThat(selected).isNotNull();
+    }
+
+    @Test
+    void selectIsDeterministicForTheSameSeed() {
+        RaritySelector firstSelector = RaritySelector.withDefaultDistribution();
+        RaritySelector secondSelector = RaritySelector.withDefaultDistribution();
+
+        Rarity firstSelected = firstSelector.select(new Random(42L));
+        Rarity secondSelected = secondSelector.select(new Random(42L));
+
+        assertThat(firstSelected).isEqualTo(secondSelected);
+    }
+
+    @Test
+    void selectOnlyReturnsKnownRaritiesAndCoversCommon() {
+        RaritySelector selector = RaritySelector.withDefaultDistribution();
+        Random random = new Random(1L);
+        Set<Rarity> selectedRarities = EnumSet.noneOf(Rarity.class);
+
+        for (int i = 0; i < 1000; i++) {
+            selectedRarities.add(selector.select(random));
+        }
+
+        assertThat(selectedRarities).isSubsetOf(Rarity.values());
+        assertThat(selectedRarities).contains(Rarity.COMMON);
+    }
+}
